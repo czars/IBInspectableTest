344bdf2c918146be5cebaa15986d990827cded68
paul <paul.chou.dev@gmail.com>
2016-05-10 22:39:15 +0800

    Initial Commit

---
+++ IBInspectableTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IBInspectableTest
+//
+//  Created by Czars on 2016/5/10.
+//  Copyright © 2016年 Czars. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
